768a78de6773167210baff9c7d72fa623cb301d5
unknown <rachidchaf2001@gmail.com>
2024-09-05 15:21:59 +0100

    added database.js config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('blog_db', 'your_username', 'your_password', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    dialect: 'mysql'
 });
 
 module.exports = sequelize;
