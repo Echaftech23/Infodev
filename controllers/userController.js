e1dd74d5f0ef5399b2ddb460c8d9ad6205b9ffa6
elFilaly001 <elfilalyabdeljalil@gmail.com>
2024-09-10 16:21:32 +0100

    added hash password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const getLoginPage = async(req,res)=>{
     res.render("login");
@@ -9,11 +11,18 @@ const getSignPage = async(req,res) =>{
 }
 
 const createUser = async (req , res)=>{
+
     console.log(req.body.username);
-    const {username , email , password} = req.body
-    console.log(req.body);
-    const newUser = await User.create({username , email , password});
-    // res.status(201).json(newUser);
+    if(req.body.password == req.body.Confirm_password){
+        req.body.password = await bcrypt.hash(req.body.password , 10);
+        const {username , email , password} = req.body
+        console.log(req.body);
+        const newUser = await User.create({username , email , password});
+        res.redirect("/")
+    }else{
+        res.body.message = "Please check your imformations";
+        res.redirect("/sign")
+    }
 }
 
 module.exports ={
