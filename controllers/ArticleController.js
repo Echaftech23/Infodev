fc28d54c2ebffb81c81267019a02a48e583ad509
unknown <rachidchaf2001@gmail.com>
2024-09-20 23:52:08 +0100

    merging F-article into dev.

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -203,7 +203,6 @@ class ArticleController {
             req.flash('success', 'Article deleted successfully.');
             return res.redirect("/");
         } catch (error) {
-            console.log(error);
             req.flash('error', 'An error occurred while deleting the article.');
             return res.status(500).redirect('/');
         }
